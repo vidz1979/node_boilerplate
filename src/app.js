06098008e4a5818d455cd4aae3497b2fa18bd3cd
Junior Vidotti <junior@MODAVERAO.LOCAL>
2019-12-29 21:41:12 -0400

    pequenas alteracoes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
+import 'dotenv/config'
+import cors from 'cors'
 import express from 'express'
-import { Users } from './repositories'
-
+import bodyParser from 'body-parser'
 const app = express()
 
+import User from './repositories/User'
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Desativa o X-Powered-By: Express
+app.disable('x-powered-by')
+
+// Criamos uma rota raiz com o texto Hello World!
 app.get('/', (req, res) => {
-  let users = Users.getUsers()
-  return res.json({ users })
+  res.send('Hello World!')
 })
 
-app.listen(3333)
+// GET /users
+app.get('/users', (req, res) => {
+  let users = User.getUsers()
+  return res.json(users)
+})
+
+// POST /users
+app.post('/users', (req, res) => {
+  if (req.body.name) {
+    if (User.addUser(req.body.name))
+      return res.status(201).json({ message: 'Usuário criado.' })
+    else return res.status(400).json({ message: 'Usuário já existe.' })
+  } else {
+    return res.status(400).json({ message: 'Dados incorretos.' })
+  }
+})
+
+// Passamos a porta onde o servidor ficará ouvindo
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port: ${process.env.PORT}`)
+})
--- src/repositories/User.js
+++ src/repositories/User.js
@@ -1,5 +1,15 @@
-const users = ["Junior", "Alexandra", "Carol", "Sophia"]
+const users = ['Junior', 'Alexandra', 'Carol', 'Sophia']
 
-export function getUsers() {
+const getUsers = () => {
   return users
 }
+
+const addUser = name => {
+  const duplicate = users.find(user => user == name)
+  if (!duplicate) {
+    users.push(name)
+  }
+  return !duplicate
+}
+
+export default { getUsers, addUser }
